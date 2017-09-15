1468ad60eb9a1a34112fb1cf5d24f87c6ff98e62
Char-kun <panduhbyte@gmail.com>
2017-09-15 03:35:41 -0400

    Clean up. Pre-ppm.
    Made it pretty before cont to ppm.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main() {
-    // ### Takes in file. ###
+    // ### Takes in file as string vector then convert to integer. ###
     ifstream infile;
     infile.open("integers.txt");
     vector<string> numStr;
@@ -27,45 +27,26 @@ int main() {
     infile.close();
     infile.clear();
 
-    cout << numStr.front() << endl;
-    cout << numStr.back() << endl;
-    cout << numStr.at(3) << endl;
-
     int numSize = numStr.size();
     for(int a=0; a < numSize; a++){
         int n = atoi(numStr.at(a).c_str());
         numVec.push_back(n);
     }
 
-    cout << numStr.front() << endl;
-    cout << numStr.back() << endl;
-    cout << numStr.at(3) << endl;
-    cout << numSize << endl;
 
-
-    /*
-
-    // ### Integers put into vector, then made into an integer array. ###
+    // ### Vector integer converted to integer array then find max. ###
     int* numInt = &numVec[0];
 
     int max = numInt[0];
     int min = numInt[0];
-    int numSize = numVec.size();
 
-    for (int i = 0; i < numSize; i++)
-    {
-        if (numInt[i] > max)
-        {
-            max = numInt[i];
-        }
-        else if (numInt[i] < min)
-        {
-            min = numInt[i];
-        }
+    for (int i = 0; i < numSize; i++) {
+        if (numInt[i] > max) {max = numInt[i];}
+        else if(numInt[i] < min) {min = numInt[i];}
     }
 
     // ### Found min and max values within the array. ###
 
-     */
+
     return 0;
 }
