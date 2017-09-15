31235e886b214669c96aa2b46e98661dc6333a4e
Char-kun <panduhbyte@gmail.com>
2017-09-15 01:01:32 -0400

    Made newfile.txt
    Tried to figure out where the resource files are places within CLion. So I made a file. Then dropped all the resources I'll need within cmake-build-debug

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     ifstream infile;
-    infile.open("F:\\Code_Projects\\P2_MtPath\\integers.txt");
+    infile.open("integers.txt");
 
     if(infile.fail()){
         cout<<"Oops\n";
@@ -19,5 +19,14 @@ int main() {
             cout<<s<<endl;
         }
     }
+    infile.close();
+    infile.clear();
+
+    ofstream outfile;
+    outfile.open("newfile.txt");
+    outfile<<"Words. Words.\nWords.";
+    outfile.close();
+    outfile.clear();
+
     return 0;
 }
