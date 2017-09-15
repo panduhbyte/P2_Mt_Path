91f978d55de4c743838b55055b44f5c51ff8f289
Charles Valdez <cvldz03@gmail.com>
2017-09-14 21:39:27 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/P2_MtPath.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2_MtPath.dir/main.cpp.obj"
+  "P2_MtPath.pdb"
+  "P2_MtPath.exe"
+  "P2_MtPath.exe.manifest"
+  "libP2_MtPath.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P2_MtPath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
