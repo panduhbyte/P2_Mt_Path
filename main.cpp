af66eb4e8d1edc088047ecf9b4fe625317208c6e
Char-kun <panduhbyte@gmail.com>
2017-09-14 22:31:12 -0400

    Input file
    Added logic to take in an int. Not tested.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include<string>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+
+    ifstream inFile;
+    inFile.open("integers.txt");
+
+    int cntInt;
+    inFile >> cntInt;
+    int numb[cntInt];
+    for(int a = 0; a < cntInt; a++) inFile >> numb[a];
+
+
     return 0;
 }
